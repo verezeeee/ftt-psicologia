0f4dff3c6cfcf2ff31b997adab08e7cd0a076ce0
Ricardo Fonseca <ricardofsdomene@icloud.com>
2023-04-20 16:47:12 -0300

    ChakraUI implementation

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, useState, useContext, ReactNode } from "react";
+
+// Interface do usuário
+interface User {
+  email: string;
+  name?: string;
+}
+
+interface ISignInRequest {
+  email: string;
+  password: string;
+}
+
+interface ISignInResponse {
+  user?: {
+    name: string;
+    email: string;
+  };
+  token?: string;
+  error?: string;
+}
+
+// Interface do contexto
+interface AuthContextData {
+  user: User | null;
+  signIn: (credentials: ISignInRequest) => Promise<ISignInResponse>;
+  signOut: () => void;
+}
+
+// Interface das props
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+// Contexto
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+// Provider
+export function AuthContextProvider({ children }: AuthProviderProps) {
+  // Estado do usuário
+  const [user, setUser] = useState<User | null>({
+    email: "ricardofsdomene@icloud.com",
+    name: "Ricardo Fonseca",
+  });
+
+  // Lógica de autenticação
+  async function signIn({ email, password }: ISignInRequest) {
+    return {
+      error: "Usuário ou senha inválidos",
+    };
+  }
+
+  // Lógica de logout
+  function signOut() {
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export default AuthContext as React.Context<AuthContextData>;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+
+import type { AppProps } from "next/app";
+import { AuthContextProvider } from "../../contexts/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthContextProvider>
+  );
 }
